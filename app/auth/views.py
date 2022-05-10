7acec0ee554170095b70f64e2a13d24eb319500c
KakushaVenecia <kakushavenecia@gmail.com>
2022-05-11 02:39:06 +0300

    This will restyle the home display view

--- app/auth/views.py
+++ app/auth/views.py
@@ -30,7 +30,7 @@ def register():
         db.session.add(user)
         db.session.commit()
  
-        # mail_message("Welcome to Pitcher App!","email/email",user.email,user=user)
+        mail_message("Welcome to Pitcher App!","email/email", user.email, user=user)
         return redirect(url_for('auth.login'))
     title = "New Account"
     return render_template('auth/register.html',registration_form = form, title=title)
