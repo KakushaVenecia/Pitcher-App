3dad36786cd4cb1f2a13f7697690bdad1f39cac5
KakushaVenecia <kakushavenecia@gmail.com>
2022-05-11 01:20:16 +0300

    Updated the file

--- manage.py
+++ manage.py
@@ -1,5 +1,8 @@
 from app import create_app , db
+from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, Server
+from app.models import User
+
 
 # Creating app instance
 app = create_app('development')
@@ -7,11 +10,17 @@ app = create_app('development')
 manager = Manager(app)
 manager.add_command('server',Server)
 
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+
 @manager.shell
 def make_shell_context():
-    return dict(app= app, db = db)
+    return dict(app= app, db = db ,User = User)
 
 if __name__ == '__main__':
+    app.config['SECRET_KEY']='hello'
     manager.run()
+
 
 
