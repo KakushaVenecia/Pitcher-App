e05f762230c3a1df0bf137a35b70a28bef11f9aa
KakushaVenecia <kakushavenecia@gmail.com>
2022-05-11 01:18:55 +0300

    Updated configuration class

--- config.py
+++ config.py
@@ -1,15 +1,26 @@
+import os
+
 
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://venecia:12345@localhost/pitcherapp'
-    SECRET_KEY = 'os.eviron'
+    SECRET_KEY =os.environ.get('SECRET_KEY')
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME= os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD= os.environ.get('MAIL_PASSWORD')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
 
 class ProdConfig(Config):
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://venecia:12345@localhost/pitcherapp'
     pass
 
 class TestConfig(Config):
     pass
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://venecia:12345@localhost/pitcherapp'
     DEBUG = True
 
 config_options ={
